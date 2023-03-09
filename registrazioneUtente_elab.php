1d2df0b3ddb59074a9625c011353bfcaf9f5d2dd
Gianni0177 <gianninacu0@gmail.com>
2023-03-09 15:52:41 +0100

    Update registrazioneUtente_elab.php

---
+++ registrazioneUtente_elab.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+    <title>ELAB</title>
+    <style>
+        body{
+            background-color: beige;
+        }
+    </style>
+</head>
+<body>
+    <center>
+        <h1><b>ELAB</b></h1>
+
+        <?php
+        echo "Nominativo = ". $_POST['Nominativo'] ."<br>";
+        ?>
+    </center>
+</body>
+</html>
